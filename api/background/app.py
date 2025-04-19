bc4b44a74ff7c6e82eb5aa9c1ace0bf6ce56aa12
_kitg <gerasimovkit@gmail.com>
2025-04-19 16:12:41 -0400

    feat: background function finished

--- api/background/app.py
+++ api/background/app.py
@@ -1,25 +1,74 @@
-from firebase_admin import credentials, firestore, auth, initialize_app
-from firebase_admin.auth import UserNotFoundError
+import json
+import os
+from datetime import datetime, timezone
 
-cred = credentials.Certificate('firebase.json')
-initialize_app(cred)
-db = firestore.client()
+import boto3
+from dynamo import db
+
+table = os.environ['WORKOUTS_TABLE']
+avatar_bucket = os.environ['MEDIA_BUCKET']
+auth_function = os.environ['AUTH_FUNCTION']
+
+s3 = boto3.client('s3')
+lambda_ = boto3.client('lambda')
 
 
 def delete_account(user_id: str):
-    try:
-        auth.delete_user(user_id)
-        print(f'Deleted user {user_id} from Firebase Auth')
-    except UserNotFoundError:
-        pass
-    except Exception as e:
-        print(f'Error deleting user {user_id} from Firebase Auth: {e}')
+    delete_avatar(user_id)
+    print(f'Deleted avatar from {avatar_bucket}')
 
+    delete_from_table(user_id)
+    print(f'Deleted account from {table}')
+
+
+def delete_from_table(account_id: str) -> dict:
+    db().delete_item(
+        TableName=table,
+        Key={
+            'PK': {'S': f'USER#{account_id}'},
+            'SK': {'S': 'ACCOUNT'}
+        },
+    )
+
+    return db().update_item(
+        TableName=table,
+        Key={
+            'PK': {'S': f'USER#{account_id}'},
+            'SK': {'S': 'ACCOUNT'}
+        },
+        UpdateExpression='SET #ts = :deletedAt',
+        ExpressionAttributeNames={
+            '#ts': 'deletedAt',
+        },
+        ExpressionAttributeValues={
+            ':deletedAt': {'S': datetime.now(timezone.utc).isoformat()},
+        },
+    )
+
+
+def delete_avatar(account_id: str) -> dict:
+    return s3.delete_object(
+        Bucket=avatar_bucket,
+        Key=f'avatars/{account_id}',
+    )
+
+
+def call_lambda(function_name: str, event: dict) -> dict | None:
     try:
-        db.collection('users').document(user_id).delete()
-        print(f'Deleted Firestore document for user {user_id}')
-    except Exception as e:
-        print(f'Error deleting user {user_id} from Firestore: {e}')
+        body = json.dumps(event).encode('utf-8')
+        response = lambda_.invoke(
+            FunctionName=function_name,
+            InvocationType='RequestResponse',
+            Payload=body,
+        )
+
+        payload = response['Payload'].read()
+        return json.loads(payload.decode('utf-8'))
+    except json.JSONDecodeError as error:
+        if error.pos == 0:
+            return {}
+    except Exception:
+        raise
 
 
 def handler(event: dict, context) -> dict:
@@ -31,7 +80,8 @@ def handler(event: dict, context) -> dict:
             'Payload': {'user_id': user_id},
         }:
             delete_account(user_id)
-
+            r = call_lambda(auth_function, event)
+            print(f'On deleting {user_id} from Firebase Auth: {r}')
     try:
         return {'statusCode': 200}
     except Exception as e:
